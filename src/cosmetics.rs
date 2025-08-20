86a66e0d603a4f5dc687f499f07a4b73e4110533
Leo dev <klesti.selimay@icloud.com>
2025-08-21 00:45:51 +0200

    amg petronas and ferrari with redbull

--- src/cosmetics.rs
+++ src/cosmetics.rs
@@ -1,3 +1,3 @@
-pub const CLOAKS: &[&str] = &["mercedes_flow", "glitch", "crimson_mark", "bmw"];
+pub const CLOAKS: &[&str] = &["mercedes_flow", "glitch", "crimson_mark", "bmw", "amg", "amg_petronas", "ferrari", "redbull"];
 
 pub const HATS: &[&str] = &["horns_black", "horns_white", "halo", "halo_black"];
