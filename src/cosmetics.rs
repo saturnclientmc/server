3b387033edf122101be96be181b14963f0a337dd
Leo dev <klesti.selimay@icloud.com>
2025-09-03 04:33:52 +0200

    Black hole cloaks

--- src/cosmetics.rs
+++ src/cosmetics.rs
@@ -1,3 +1,3 @@
-pub const CLOAKS: &[&str] = &["mercedes_flow", "glitch", "crimson_mark", "bmw", "amg", "amg_petronas", "ferrari", "redbull"];
+pub const CLOAKS: &[&str] = &["mercedes_flow", "glitch", "crimson_mark", "bmw", "amg", "amg_petronas", "ferrari", "redbull", "black_hole_amethyst", "black_hole_flame", "black_hole_white"];
 
 pub const HATS: &[&str] = &["horns_black", "horns_white", "halo", "halo_black"];
